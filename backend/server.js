ce115a44a2cbbcc67ed83516acdaf3426b32d062
abhishek <abhishekpl8219@gmail.com>
2025-09-29 21:42:30 +0530

    GEt all users controllers.

--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,7 @@ import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
 import authRoutes from "./routes/auth.routes.js";
 import messageRoutes from "./routes/message.routes.js";
+import userRoutes from "./routes/users.routes.js";
 import connectToMongoDB from "./db/connectToMOngoDB.js";
 import Message from "./models/message.model.js";
 
@@ -13,6 +14,7 @@ app.use(express.json());
 app.use(cookieParser());
 app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
+app.use("/api/users/", userRoutes);
 
 app.listen(PORT, () => {
   connectToMongoDB();
